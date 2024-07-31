50ffd715c7b26f04532a67de8281caf356a28791
Adri168 <cristayce@gmail.com>
2024-07-31 08:15:25 -0600

    puto

--- FronEndProyecto/vistas/secciones/seccionCuatro.xaml.cs
+++ FronEndProyecto/vistas/secciones/seccionCuatro.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FronEndProyecto.vistas.subtemas;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,11 +16,44 @@ namespace FronEndProyecto.vistas.secciones
         public seccionCuatro()
         {
             InitializeComponent();
+            CargarLeccion();
         }
 
-        private void LeccionesListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        private void CargarLeccion()
         {
+            var less = new List<Less>
+            {
+                new Less {  NameLess = "Lección 1: Software de Gestión Financiera", Page = typeof(sub1)},
+                new Less {  NameLess = "Lección 2: Método del Valor Ganado", Page = typeof(sub2)},
+                new Less {  NameLess = "Lección 3: Análisis de Brechas", Page = typeof(sub3)},
+                new Less {  NameLess = "Lección 4: TPresupuestación Basada en Actividades", Page = typeof(sub4)},
+                // Agrega más lecciones aquí
+            };
 
+            // Configurar el DataTemplate directamente en el código detrás
+            LessList.ItemTemplate = new DataTemplate(() =>
+            {
+                var textCell = new TextCell();
+                textCell.SetBinding(TextCell.TextProperty, "NameLess");
+                return textCell;
+            });
+
+            LessList.ItemsSource = less;
         }
+
+        private async void OnLeccTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item != null && e.Item is Less leccion)
+            {
+                // Navegar a la página de la lección
+                await Navigation.PushAsync((Page)Activator.CreateInstance(leccion.Page));
+            }
+        }
+    }
+
+    public class Less
+    {
+        public string NameLess { get; set; }
+        public Type Page { get; set; }
     }
 }
---
+++ FronEndProyecto/vistas/subtemas/sub2.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FronEndProyecto.vistas.subtemas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class sub2 : ContentPage
+    {
+        public sub2()
+        {
+            InitializeComponent();
+        }
+    }
+}
